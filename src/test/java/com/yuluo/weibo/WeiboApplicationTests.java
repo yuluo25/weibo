427c6bafda81880177a4ee4721e064e4e9e2e06d
Yuluo <yuluo25@gmail.com>
2022-03-03 10:58:23 +0800

    Create project

---
+++ src/test/java/com/yuluo/weibo/WeiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuluo.weibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
